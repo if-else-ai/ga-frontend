3186cc83511f593ee8a4647702adf24436ea998e
Zebra <Thirawatp9@gmail.com>
2021-09-11 23:59:56 +0700

    📝 Route Pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Reproduce from "./contents/Reproduce";
+import About from "./contents/About";
+import Home from "./contents/Home";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <h1>Setting up</h1>
-      <h2>ทดสอบ</h2>
-      <h2>test</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/Reproduce">
+          <Reproduce />
+        </Route>
+        <Route exact path="/About">
+          <About />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/contents/Reproduce.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Reproduce extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Reproduce</h1>
+      </div>
+    );
+  }
+}
+
+export default Reproduce;
