ad147bdb4a5e28274e88da0e0b7870475005ed9c
Zebra <Thirawatp9@gmail.com>
2021-09-11 21:50:22 +0700

    ✨ Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Setting up</h1>
       <h2>ทดสอบ</h2>
       <h2>test</h2>
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Reproduce Image",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "About",
+    url: "#",
+    cName: "nav-links",
+  },
+];
