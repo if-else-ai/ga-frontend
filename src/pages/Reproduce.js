24fe13577fefe628fac7b17dad8deb17c5c2ec89
Natthadecha Kijsamrej <41380751+Albert1647@users.noreply.github.com>
2021-09-12 23:31:21 +0700

    Update Reproduce Image Page

--- src/pages/Reproduce.js
+++ src/pages/Reproduce.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Card from "../components/Card";
+import Pageheader from "../components/Pageheader"
 
 class Reproduce extends Component {
   render() {
-    return <div className="page">รูปป</div>;
+    return (
+      <div className="page">
+        <Pageheader />
+        <Card />
+      </div>
+    );
   }
 }
 
