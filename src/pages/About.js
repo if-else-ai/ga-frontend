9e89b5b0c8d229c993cb52ebd1a9158916d8a799
fffahchu <fahchuti.43@gmail.com>
2021-09-13 20:58:41 +0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 import "./pages.css";
 
 class About extends Component {
-  render() {
-    return (
-      <div className="page">
-        <h1>โหลเทส</h1>
-        about อะเบ้าาา
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = "page" >
+            <
+            h1 > ฟ้ าทำอะเบ้ า < /h1>
+            about อะเบ้ าาา <
+            /div>
+        );
+    }
 }
 
 export default About;
