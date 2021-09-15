5de6217d953a06a35516e6a5ee74a980d688f25e
fffahchu <fahchuti.43@gmail.com>
2021-09-15 15:51:56 +0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import "./pages.css";
 
 class About extends Component {
+<<<<<<< HEAD
     render() {
         return ( <
             div className = "page" >
             <
-            h1 > ฟ้ าทำอะเบ้ า < /h1>
-            about อะเบ้ าาา <
+            h1 > OBJECTIVE < /h1>{" "} <
+            h3 > Create a new image using Genetic Algorithm. < /h3>{" "} <
+            h3 > Research and understanding how Genetic Algorithm works. < /h3>{" "} <
             /div>
         );
     }
+=======
+  render() {
+    return (
+      <div className="page">
+        <h1>โหลเทส</h1>
+        about อะเบ้าาา
+      </div>
+    );
+  }
+>>>>>>> b77b84c58832980b89513cc52ad27fb55e0f6f12
 }
 
 export default About;
