bf4aeed97633c99f0870ad25b9e4175a3c4b4c06
mcca123 <mccab9@gmail.com>
2021-09-17 18:46:06 +0700

    home 2

--- src/components/Home2/Home2.js
+++ src/components/Home2/Home2.js
@@ -2,7 +2,28 @@ import React from "react";
 import "./Home2.css";
 
 function Home2() {
-  return <div className="container">section2</div>;
+  return (
+    <div className="container">
+      <div className="container-text">
+        <h1>What is Genetic Algorithm?</h1>
+        <p>
+          Genetic algorithms are a class of numerical and combinational
+          optimizers which are especially useful for solving complex nonlinear
+          and nonconvex problems. Effort to apply genetic algorithms to large
+          scale and latency-sensitive problems has been made in Ref. Pipelined
+          genetic propagation is proposed to map the algorithm to DFEs
+          efficiently and address the challenge in large shared population
+          memory and intrinsic loop-carried dependency. Such DFE-oriented
+          approach is intrinsically distributed and pipelined. The genetic
+          algorithm solver is represented as a graph of loosely coupled genetic
+          operators, which allows the solution to be scaled to the available
+          resources and to dynamically change topology at run-time to explore
+          different solution strategies. Experiments show the DFE is effective
+          in accelerating different applications that apply genetic algorithms.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home2;
