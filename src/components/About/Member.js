e5c16e28b68c521f7dec84f93463e0cbf69587b3
Natthadecha Kijsamrej <41380751+Albert1647@users.noreply.github.com>
2021-09-19 08:20:44 +0700

    🔨 Update Member Card / Clean code

--- src/components/About/Member.js
+++ src/components/About/Member.js
@@ -1,29 +1,47 @@
 import "./Member.css";
-import { Team } from "./team-info"
-import Card from "./Card"
+import { Team } from "./team-info";
+import Card from "./Card";
 
-	const Member = () => {
-		// let info = [
-		// 	"/images/contributor/Natthadecha.jpg"
-		// ];
+const Member = () => {
+	// let info = [
+	// 	"/images/contributor/Natthadecha.jpg"
+	// ];
 
-		return (
-			<div className="member">
-				<h1> DEVELOPER </h1>
-				<div className="member__items">
-					{Team.map((item, index) => {
-						return (
-							<div className="member__item">
-								<Card>
-									<img src={item.image} alt="team" /> 
-									<p>{item.name}</p>
-								</Card>
-							</div>
-						);
-					})}
-				</div>
+	return (
+		<div className="member">
+			<h1> DEVELOPER </h1>
+			<div className="member__items">
+				{Team.map((item, index) => {
+					return (
+						<div className="member__item" key={index}>
+							<Card >
+								<img src={item.image} alt="team" />
+								<div className="card-text">
+									<p>Name : {item.name}</p>
+									<p>Role : </p>
+								</div>
+								<div className="chip__items">
+									{item.role.map((item, index) => {
+										return (
+											<div className="chip__item" key={index}>
+												<span>
+													{item}
+													<br />
+												</span>
+											</div>
+										);
+									})}
+								</div>
+								<div className="card-id">
+									{item.id}
+								</div>
+							</Card>
+						</div>
+					);
+				})}
 			</div>
-		);
-	}
+		</div>
+	);
+};
 
 export default Member;
--- src/components/About/team-info.js
+++ src/components/About/team-info.js
@@ -1,19 +1,50 @@
 export const Team = [
-  {
-    name: "Natthadecha Kijsamrej",
-    image: "/images/contributor/Natthadecha.jpg",
-  },
-  {
-    name: "Natthadecha Kijsamrej",
-    image: "/images/contributor/Natthadecha.jpg",
-  },
-  {
-    name: "Natthadecha Kijsamrej",
-    image: "/images/contributor/Natthadecha.jpg",
-  },
-  {
-    name: "Natthadecha Kijsamrej",
-    image: "/images/contributor/Natthadecha.jpg",
-  },
-
+	{
+		id: "62050133",
+		name: "Kunasin Charusiriwat",
+		role: [],
+		image: "/images/contributor/Natthadecha.jpg",
+	},
+	{
+		id: "62050134",
+		name: "Jatuphol Chailittikun",
+		role: [],
+		image: "/images/contributor/Natthadecha.jpg",
+	},
+	{
+		id: "62050146",
+		name: "Chutiyakan Oonjon",
+		role: [],
+		image: "/images/contributor/Natthadecha.jpg",
+	},
+	{
+		id: "62050149",
+		name: "Noppawat Boonma",
+		role: [],
+		image: "/images/contributor/Natthadecha.jpg",
+	},
+	{
+		id: "62050155",
+		name: "Natthadecha Kijsamrej",
+		role: ["Front-end", "Algorithm", "Front-end Teacher"],
+		image: "/images/contributor/Natthadecha.jpg",
+	},
+	{
+		id: "62050163",
+		name: "Thirawat Phongsathatkit",
+		role: [],
+		image: "/images/contributor/Natthadecha.jpg",
+	},
+	{
+		id: "62050190",
+		name: "Praphat Wachiramateekul",
+		role: [],
+		image: "/images/contributor/Natthadecha.jpg",
+	},
+	{
+		id: "62050191",
+		name: "Punnarat Rattanapawan",
+		role: [],
+		image: "/images/contributor/Natthadecha.jpg",
+	},
 ];
