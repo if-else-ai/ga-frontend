3704f3e46b7a583697a9dd6a956e102dd3c37f69
Zebra <Thirawatp9@gmail.com>
2021-09-20 15:28:00 +0700

    ➖ Remove unused video and image

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -4,26 +4,24 @@ import { Button } from "./Button";
 import "./HeroSection.css";
 
 const HeroSection = () => {
-	return (
-		<div className="hero-container">
-			{/* {/* <video src="/videos/video-1.mp4" autoPlay loop muted />{" "} */}
-			{/* ใส่vdoพื้นหลังแล้วมันไม่แสดงอะ */}
-			<div id="video-container">
-				<video src="/videos/video-2.mp4" autoPlay loop muted />
-			</div>
-			<h1>Reproduce Image</h1>
-			<p>with Genetic Algorithm</p>
-			<div className="hero-btns">
-				<Button
-					className="btns"
-					buttonStyle="btn--primary"
-					buttonSize="btn--large"
-				>
-					Get Started
-				</Button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="hero-container">
+      <div id="video-container">
+        <video src="/videos/video-2.mp4" autoPlay loop muted />
+      </div>
+      <h1>Reproduce Image</h1>
+      <p>with Genetic Algorithm</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          Get Started
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
