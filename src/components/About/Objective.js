f3d4a6a8ac6cff827a2d9d32af86ef1a55b032f5
fffahchu <fahchuti.43@gmail.com>
2021-10-31 20:52:01 +0700

    Update Objective.js

--- src/components/About/Objective.js
+++ src/components/About/Objective.js
@@ -7,11 +7,28 @@ const Objective = () => {
 			<h1> OBJECTIVE </h1>
 			<ul>
 				<li>
-					<h3> Create a new image using Genetic Algorithm. </h3>
+					<h3>To propose strategies for applying genetic algorithms. That have evolved working together (Co-Operative Co-Evolution Genetic Algorithm) 
+					with Technical indicators to create a stock trading strategy to focus on finding the highest return on investing in the stock market.
+					</h3>
 				</li>
 				<li>
 					<h3>
-						Research and understanding how Genetic Algorithm works
+						Research and understanding how Genetic Algorithm works.
+					</h3>
+				</li>
+				<li>
+					<h3>
+					Create a new image using Genetic Algorithm. 
+					</h3>
+				</li>
+				<li>
+					<h3>
+					To find the mathematical equations in the image transformation.
+					</h3>
+				</li>
+				<li>
+					<h3>
+					The genetic method is easy to apply and the most suitable answer can be obtained.
 					</h3>
 				</li>
 			</ul>
