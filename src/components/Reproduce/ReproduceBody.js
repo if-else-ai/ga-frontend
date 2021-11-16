a6dc760d0d85f5a6398617966e61ee52f3cee039
Natthadecha Kijsamrej <41380751+Albert1647@users.noreply.github.com>
2021-11-17 03:04:43 +0700

    Change Display

--- src/components/Reproduce/ReproduceBody.js
+++ src/components/Reproduce/ReproduceBody.js
@@ -7,7 +7,6 @@ import "./Button.css";
 import axios from "../../axios.js";
 
 const ReproduceBody = () => {
-
 	// target hidden input
 	const fileInputRef = useRef(null);
 
@@ -17,15 +16,17 @@ const ReproduceBody = () => {
 	let [split, setSplit] = useState(0);
 	let [imageURL, setImageURL] = useState("");
 	let [filename, setFilename] = useState("");
-	let [fitness, setFitness] = useState(0)
-	let [generated, setGenerated] = useState(0)
-	let [status, setStatus] = useState('Waiting')
-	let [imageArray, setImageArray] = useState([])
-	let [imageAnimationIndex, setImageAnimationIndex] = useState(0)
-	let [finishGenerate, setFinishGenerate] = useState(false)
-	let [bestResult, setBestResult] = useState('')
-	let animationIndex = 0
-	var intervalID
+	let [fitness, setFitness] = useState(0);
+	let [generated, setGenerated] = useState(0);
+	let [status, setStatus] = useState("Waiting");
+	let [imageArray, setImageArray] = useState([]);
+	let [imageAnimationIndex, setImageAnimationIndex] = useState(0);
+	let [finishGenerate, setFinishGenerate] = useState(false);
+	let [bestResult, setBestResult] = useState("");
+	let [isGenerating, setIsGenerating] = useState(false);
+	let animationIndex = 0;
+	var intervalID;
+	// var animationID;
 
 	// click hidden file input if button is click
 	const uploadClickHandler = (event) => {
@@ -36,8 +37,8 @@ const ReproduceBody = () => {
 	const uploadHandler = (event) => {
 		// use form0-data to store image
 		const file = event.target.files[0];
-		console.log(file)
-		if(file){
+		console.log(file);
+		if (file) {
 			let formData = new FormData();
 			let tempBlob = new Blob([file], { type: "image/jpeg" });
 			formData.append("file", tempBlob, file.name);
@@ -49,11 +50,11 @@ const ReproduceBody = () => {
 					},
 				})
 				.then((res) => {
-					console.log(res)
-					filename = res.data.filename
+					console.log(res);
+					filename = res.data.filename;
 					let currentImage = `${axios.defaults.baseURL}/image?filename=${filename}`;
-					setFilename(filename)
-					setImageURL(currentImage)
+					setFilename(filename);
+					setImageURL(currentImage);
 					setHaveImage(true);
 				})
 				.catch((err) => {
@@ -65,30 +66,70 @@ const ReproduceBody = () => {
 
 	// run task
 	const startHandler = () => {
+		setFinishGenerate(false);
+		setIsGenerating(true)
+		setImageArray([])
 		axios
-			.post(
-				"/run",
-				{
-					"filename": filename,
-					"generation": Number(generation),
-					"split": Number(split)
-				}
-			)
+			.post("/run", {
+				filename: filename,
+				generation: Number(generation),
+				split: Number(split),
+			})
 			.then((res) => {
-				console.log(res)
-				let taskID = res.data.task_id
+				console.log(res);
+				let taskID = res.data.task_id;
 
 				// start getting status every 2 second
 				intervalID = setInterval(() => {
-					fetchStatus(taskID)
+					fetchStatus(taskID);
 				}, 2000);
-
 			})
 			.catch((err) => {
 				console.log(err);
 			});
-
 	};
+
+	const fetchStatus = (taskID) => {
+		axios.get(`/status/${taskID}`).then((res) => {
+			console.log(res)
+			setFitness(res.data.current_fitness);
+			setGenerated(res.data.current_generation);
+			setStatus(res.data.status);
+			let imageData = res.data.sol_im;
+			let imageArray = imageData.map((imageFilename) => {
+				return `${axios.defaults.baseURL}/image?filename=${imageFilename}`;
+			});
+			setImageArray(imageArray);
+			if (
+				res.data.status === "Completed" ||
+				res.data.state === "FAILURE"
+			) {
+				axios.post(`/tasks/${taskID}`).then((res) => {
+					console.log("clear task", taskID);
+					clearInterval(intervalID);
+				});
+				let finishDataString = res.data.result;
+				finishDataString = finishDataString.split(" ");
+				let result = {
+					bestFit: finishDataString[0],
+					bestFitGeneration: finishDataString[2],
+				};
+				setBestResult(result);
+
+				
+
+				setFinishGenerate(true);
+				setIsGenerating(false)
+
+				// animationID = setInterval(() => {
+				setInterval(() => {
+					playAnimation();
+				}, 1000);
+			}
+		});
+	};
+
+
 
 	const generationHandler = (event) => {
 		setGeneration(event.target.value);
@@ -98,74 +139,32 @@ const ReproduceBody = () => {
 		setSplit(event.target.value);
 	};
 
-	const fetchStatus = (taskID) => {
-		axios.get(`/status/${taskID}` ).then(
-			(res) => {
-				setFitness(res.data.current_fitness)
-				setGenerated(res.data.current_generation)
-				setStatus(res.data.status)
-				console.log('array',res.data.sol_im)
-				let imageData = res.data.sol_im
-				let imageArray = imageData.map(
-					imageFilename => {
-						return `${axios.defaults.baseURL}/image?filename=${imageFilename}`}
-				)
-				console.log(imageArray)
-				setImageArray(imageArray)
-				console.log(res)
-				if(res.data.status === 'Completed' || res.data.state === "FAILURE"){
-					console.log('res', res.data.result)
-					let finishData = res.data.result
-					finishData = finishData.split(' ')
-					let Data = {
-						bestFit: finishData[0],
-						bestFitGeneration: finishData[2]
-					}
-					setBestResult(Data)
-
-					axios.post(`/tasks/${taskID}`).then(
-						res => {
-							console.log('clear', taskID)
-						}
-					)
-					clearInterval(intervalID)
-					setFinishGenerate(true)
-					setInterval(()=>{
-						playAnimation()
-					},1000)
-				}
-				
-			}
-		)
+	const playAnimation = () => {
+		animationIndex += 1;
+		setImageAnimationIndex(imageAnimationIndex + animationIndex);
 	};
 
+	let animation = <p>Animation: waiting</p>;
 
-	
-
-
-	const playAnimation = () => {
-		animationIndex += 1
-		setImageAnimationIndex(imageAnimationIndex + animationIndex)
-	}
-
-	let animation = (
-		<p>Animation: waiting</p>
-	)
-	
-	if(finishGenerate){
-		animation = 
-		(
+	if (finishGenerate) {
+		animation = (
 			<div className="image__animation-container">
 				<p>Progression Animation</p>
-				<ImageCard  item={imageArray[imageAnimationIndex % imageArray.length]} generation={(generation/split)*((imageAnimationIndex % imageArray.length) + 1)}/>
+				<ImageCard
+					item={imageArray[imageAnimationIndex % imageArray.length]}
+					generation={
+						(generation / split) *
+						((imageAnimationIndex % imageArray.length) + 1)
+					}
+				/>
 				<p></p>
 			</div>
-		)
+		);
 	}
 
 	let generatedImage = <p></p>;
 
-	if(finishGenerate || imageArray.length > 0){
+	if (finishGenerate || imageArray.length > 0) {
 		generatedImage = (
 			<section>
 				<p>Generated Image</p>
@@ -175,38 +174,36 @@ const ReproduceBody = () => {
 							<ImageCard
 								key={index}
 								item={item}
-								generation={(generation/split)*(index + 1)}
+								generation={(generation / split) * (index + 1)}
 							/>
 						);
 					})}
 				</div>
 			</section>
-		)
+		);
 	}
 
 	let bestFitness = <p></p>;
-	let bestGeneration= <p></p>;
+	let bestGeneration = <p></p>;
 
-	if(bestResult){
-		bestFitness = (<p>Best Fitness is: {bestResult.bestFit}</p>
-						);
+	if (bestResult) {
+		bestFitness = <p>Best Fitness is: {bestResult.bestFit}</p>;
 	}
 
-	if(bestResult){
-		bestGeneration = (<p>Best Fitness Generation: {bestResult.bestFitGeneration}</p>
-						);
+	if (bestResult) {
+		bestGeneration = (
+			<p>Best Fitness Generation: {bestResult.bestFitGeneration}</p>
+		);
 	}
-
-
 
 	return (
 		<div className="reproduce__container">
 			<Card image={imageURL} />;
 			<section className="form__container">
-				Generation: <input type="number" onChange={generationHandler} />
-				Split: <input type="number" onChange={splitHandler} />
+				Generation:{" "}
+				<input type="number" step="1" onChange={generationHandler} />
+				Split: <input type="number" step="1" onChange={splitHandler} />
 			</section>
-
 			<div className="button-row">
 				<button className="button__upload" onClick={uploadClickHandler}>
 					Upload
@@ -218,9 +215,9 @@ const ReproduceBody = () => {
 				</button>
 
 				<button
-					disabled={!(haveImage && split > 0 && generation > 0)}
+					disabled={!(haveImage && split > 0 && generation > 0 && !isGenerating )}
 					className={
-						(haveImage && split > 0 && generation > 0)
+						haveImage && split > 0 && generation > 0 && !isGenerating
 							? "button__start"
 							: "button__disable"
 					}
@@ -229,7 +226,6 @@ const ReproduceBody = () => {
 					Start
 				</button>
 			</div>
-
 			<div>
 				<p>Current Generation: {generated}</p>
 				<p>Fitness: {fitness}</p>
@@ -237,11 +233,8 @@ const ReproduceBody = () => {
 				{bestFitness}
 				{bestGeneration}
 			</div>
-			
 			{animation}
-
 			{generatedImage}
-
 		</div>
 	);
 };
